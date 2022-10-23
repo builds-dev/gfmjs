2b9940f2635cffec2423edd07c9425b598f7933c
Joseph Dykstra <1833684+ArtskydJ@users.noreply.github.com>
2022-10-22 20:37:56 -0500

    Make the tests cross-platform (#17)
    https://stackoverflow.com/questions/46745014/alternative-for-dirname-in-node-js-when-using-es6-modules#comment122817541_66651120

--- test/ignores-node/ignores-node.spec.js
+++ test/ignores-node/ignores-node.spec.js
@@ -1,7 +1,8 @@
 import { test } from 'zora'
 import path from 'path'
+import { fileURLToPath } from 'url'
 import { verify_file } from '../../src/verify_file.js'
-const __dirname = path.dirname(new URL(import.meta.url).pathname)
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 test('ignores-node', async t => {
 	const { code, failed, output } = await verify_file(path.join(__dirname, './ignores-node.md'))
