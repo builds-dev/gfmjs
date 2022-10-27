c7d348983ae791da10046fd0a1bf5fdd827cea00
ArtskydJ <1833684+ArtskydJ@users.noreply.github.com>
2022-10-26 21:39:32 -0500

    Add failing tests for source-mapped line numbers

--- test/bin/bin.spec.js
+++ test/bin/bin.spec.js
@@ -37,8 +37,18 @@ test('fail', async t => {
 	t.ok(/'something else'/.test(stderr), 'stderr contains actual value')
 })
 
-test('failed assertions are source-mapped to the markdown file', async t => {
-	const { err, stdout, stderr } = await run_bin_on_relative_file('./fail.md')
+const assert_source_map_line = async (t, relative_file, line) => {
+	const { err, stdout, stderr } = await run_bin_on_relative_file(relative_file)
 
-	t.ok(/fail\.md:\d+:\d+/.test(stderr), 'Error line points to markdown file')
+	t.ok(/\.md:\d/.test(stderr), 'Error line points to the markdown file')
+
+	const [ , line_number ] = stderr.match(/\.md:(\d+)/) || []
+	t.equal(line_number, line.toString(), `Error line is correct for ${relative_file}`)
+}
+
+test('correct source maps', async t => {
+	await assert_source_map_line(t, './fail.md', 3)
+	await assert_source_map_line(t, '../arrow-implied-return/arrow-implied-return.md', 4)
+	await assert_source_map_line(t, '../html-comment/html-comment.md', 3)
+	await assert_source_map_line(t, '../inline/inline.md', 4)
 })
